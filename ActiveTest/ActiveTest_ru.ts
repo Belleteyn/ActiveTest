0fb104c04f2dfb55a2a609cbaaeb5bdbdb124330
Timur Nasybullin <human12177@gmail.com>
2016-06-10 05:37:40 +0300

    Добавлен перевод

---
+++ ActiveTest/ActiveTest_ru.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="14"/>
+        <source>SMSClient</source>
+        <translation>SMS Клиент</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="97"/>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="110"/>
+        <source>Title</source>
+        <translation>Титровальный элемент</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="112"/>
+        <location filename="main.qml" line="119"/>
+        <source>active</source>
+        <translation>активен</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="112"/>
+        <location filename="main.qml" line="119"/>
+        <source>not active</source>
+        <translation>не активен</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="117"/>
+        <source>Server</source>
+        <translation>Сервер</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="129"/>
+        <source>Current message</source>
+        <translation>Текущее сообщение</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="143"/>
+        <source>id</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="146"/>
+        <source>priority</source>
+        <translation>приоритет</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="149"/>
+        <location filename="main.qml" line="169"/>
+        <source>message</source>
+        <translation>сообщение</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="157"/>
+        <source>Log</source>
+        <translation>Лог</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="167"/>
+        <source>time</source>
+        <translation>время</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="168"/>
+        <source>tag</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
